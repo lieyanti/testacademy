05d9fb026be7ae3b6ed52d95191ce95c6bfa1a21
lie yanti <lieyanti@10-60-49-143.0200.bsd.id.iosda.org>
2018-07-04 15:54:15 +0700

    nah

---
+++ GITcollab/Register.swift
@@ -0,0 +1,38 @@
+//
+//  Register.swift
+//  GITcollab
+//
+//  Created by lie yanti on 04/07/18.
+//  Copyright © 2018 yanti. All rights reserved.
+//
+
+import UIKit
+
+class Register: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
