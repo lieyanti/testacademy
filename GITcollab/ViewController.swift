602ceca80821390664bcddff3408e4294387d449
lie yanti <lieyanti@10-60-49-143.0200.bsd.id.iosda.org>
2018-07-04 14:36:08 +0700

    GIT collar

--- GITcollab/ViewController.swift
+++ GITcollab/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
