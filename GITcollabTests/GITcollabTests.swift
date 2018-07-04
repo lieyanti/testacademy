02b7d9c3a00a7b0acbbbefb72010bdf4ad0c6388
lie yanti <lieyanti@10-60-49-143.0200.bsd.id.iosda.org>
2018-07-04 14:23:36 +0700

    Initial Commit

---
+++ GITcollabTests/GITcollabTests.swift
@@ -0,0 +1,36 @@
+//
+//  GITcollabTests.swift
+//  GITcollabTests
+//
+//  Created by lie yanti on 04/07/18.
+//  Copyright © 2018 yanti. All rights reserved.
+//
+
+import XCTest
+@testable import GITcollab
+
+class GITcollabTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
